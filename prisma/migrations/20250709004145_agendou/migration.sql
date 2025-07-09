85fdf90dc2742cf171c13c7d6aae00917ea8fdbe
marcelolemes1340 <marcelinho.lemes@gmail.com>
2025-07-08 21:43:07 -0300

    v

---
+++ prisma/migrations/20250709004145_agendou/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE "Agendamento" (
+    "id" SERIAL NOT NULL,
+    "servico" TEXT NOT NULL,
+    "profissional" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "horario" TEXT NOT NULL,
+    "nome" TEXT NOT NULL,
+    "criadoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Agendamento_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Usuario" (
+    "id" SERIAL NOT NULL,
+    "nome" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "telefone" TEXT,
+    "cpf" TEXT,
+    "senha" TEXT NOT NULL,
+    "criadoEm" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Usuario_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Usuario_email_key" ON "Usuario"("email");
